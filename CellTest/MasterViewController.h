655629c17def46c97e199346cf20569ecccd1822
steven <rockboyfor@gmail.com>
2016-05-27 08:19:53 +0800

    Initial Commit

---
+++ CellTest/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CellTest
+//
+//  Created by user on 16/5/27.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
